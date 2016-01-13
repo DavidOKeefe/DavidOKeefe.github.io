b8d6cafea2cd4e0cc7952400389ef7113846eca6
David OKeefe <davidpokeefe3@gmail.com>
2016-01-13 15:53:39 -0500

    Redesign projects page to include interactive tabs

---
+++ js/tabs.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".tabs li").click(function(event) {
+    event.preventDefault();
+    $(".tabs li").removeClass("active");
+    $(this).toggleClass("active");
+  });
+  $(".tabs li a").click(function(event) {
+    var href = $(this).attr("href");
+    $("#bingo, #cheat_sheet, #256, #sift").addClass("tab-content");
+    $(href).removeClass("tab-content");
+  });
+});
